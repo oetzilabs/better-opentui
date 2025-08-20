0b572f85d3267851dd10525ee69165cb2e458a50
Özgür Isbert <oezguerisbert@gmail.com>
2025-08-20 15:07:48 +0200

    chores

--- examples/styled-text.ts
+++ examples/styled-text.ts
@@ -3,21 +3,19 @@ import * as Colors from "@opentuee/core/src/colors";
 import { setup } from "@opentuee/core/src/setup";
 import { Effect } from "effect";
 
-const program = Effect.gen(function* () {
-  return yield* setup(
-    Effect.fn(function* (cli) {
-      yield* cli.setBackgroundColor(Colors.White.make("#FFFFFF"));
-      const parentContainer = yield* cli.createElement("group");
-      const text = yield* cli.createElement("text", "Hello World", { left: 2, top: 2 });
-      const text2 = yield* cli.createElement("text", "Hello World 2", { left: 5, top: 5 });
-
-      yield* parentContainer.add(text);
-      yield* parentContainer.add(text2);
-      yield* cli.add(parentContainer);
-    }),
-  );
-});
-
 if (import.meta.main) {
-  BunRuntime.runMain(program);
+  BunRuntime.runMain(
+    setup(
+      Effect.fn(function* (cli) {
+        yield* cli.setBackgroundColor(Colors.White.make("#FFFFFF"));
+        const parentContainer = yield* cli.createElement("group");
+        const text = yield* cli.createElement("text", "Hello World", { left: 2, top: 2 });
+        const text2 = yield* cli.createElement("text", "Hello World 2", { left: 5, top: 5 });
+
+        yield* parentContainer.add(text);
+        yield* parentContainer.add(text2);
+        yield* cli.add(parentContainer);
+      }),
+    ),
+  );
 }
